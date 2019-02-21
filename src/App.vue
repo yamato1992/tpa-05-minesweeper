cf86e602d5343b81527513de3d06692b33d006a0
yamato1992 <yamato.mitsui.orleans@gmail.com>
2019-02-21 22:01:40 +0900

    add stubs for helper methods

--- src/App.vue
+++ src/App.vue
@@ -54,19 +54,52 @@ export default {
      * @return {undefined}
      */
     openTile: function(tile) {
-      // if the tiles is mined
-      //  this.$set(this.tiles[tile.row][tile.column], 'state', 'mine');
-      //  reveal all other tiles
-      // if its not mined
-      //  collect information on its neighbors
-      //  count how many mines surround the tile
-      //  if there are mines
-      //    reveal the numbers of mines that surround the tile
-      //  if there are no mines
-      //      this.$set(this.tiles[tile.row][tile.column], 'state', 'opened');
-      //    (for each neighbor)
-      //      (if the neighbor has not been opened yet)
-      //        (open the neighbor)
+      if (this.tiles[tile.row][tile.column].mined) {
+        this.tiles[tile.row][tile.column].state = 'mine';
+        // reveal all other tiles
+      } else {
+        const neighborTiles = collectNeighborsTileInfo(tile);
+        const countMines = countNeighborsMines(neighborTiles);
+        if (countMines > 0) {
+          this.tiles[tile.row][tile.column].state = 'opened';
+        } else {
+          //  if there are no mines
+          //    this.tiles[tile.row][tile.column].state = 'opened';
+          //    (for each neighbor)
+          //      (if the neighbor has not been opened yet)
+          //        (open the neighbor)
+        }
+      }
+    },
+
+    /**
+     * collect information on its neighbors
+     * @function 
+     * @param {Object} tile
+     * @return {Object} neighborTiles
+     */
+    collectNeighborsTileInfo: function(tile) {
+      // collect information on its neighbors
+    },
+
+    /**
+     * count how many mines surrond the tile
+     * @function
+     * @param {Array} neighborTiles
+     * @return {Number} countMines
+     */
+    countNeighborsMines: function() {
+      // count how many mines surrond the tile
+    },
+
+    /**
+     * reval other tiles
+     * @function
+     * @param {Object} tile
+     * @return {undefine}
+     */
+    revalOtherTiles: function(tile) {
+      // reval other tiles
     },
     setFlag: function(tile) {
       this.tiles[tile.row][tile.column].state = 'flagged';
